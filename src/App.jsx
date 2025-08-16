5b492a100223ce0c14a973aca3374939fcbd42d3
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-08-16 20:29:26 +0300

    feat: add global Context.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
+import { useGlobalContext } from './Context.jsx';
+
+const App = () => {
+	const { isSidebarOpen } = useGlobalContext();
+	console.log(isSidebarOpen);
+
 	return <h1>Strapi Submenus Clone</h1>;
-}
+};
 
 export default App;
