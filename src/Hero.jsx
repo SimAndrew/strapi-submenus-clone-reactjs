78057d3bc1c44bd3d84d8513398f738a74c1569f
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-08-18 17:50:34 +0300

    feat: add Hero styles

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,17 @@
-function Hero() {
-	return <h2>Hero</h2>;
-}
+const Hero = () => {
+	return (
+		<div className="hero-container">
+			<div className="hero-center">
+				<h1>Build modern websites with the most customizable Headless CMS</h1>
+
+				<p>
+					The open-source Headless CMS for developers that makes API creation
+					easy, and supports your favorite frameworks. Customize and host your
+					projects in the cloud or on your own servers.
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default Hero;
