be06f369da4ca81deaaab3c9ed690c6384bb8b6d
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-08-20 21:16:48 +0300

    feat: on hover navLinks open Submenu with sublinks from data.jsx, style

--- src/NavLinks.jsx
+++ src/NavLinks.jsx
@@ -1,13 +1,20 @@
 import sublinks from './data.jsx';
+import { useGlobalContext } from './Context.jsx';
 
 const NavLinks = () => {
+	const { setPageId } = useGlobalContext();
+
 	return (
 		<div className="nav-links">
 			{sublinks.map((item) => {
 				const { pageId, page } = item;
 
 				return (
-					<button key={pageId} className="nav-link">
+					<button
+						key={pageId}
+						className="nav-link"
+						onMouseEnter={() => setPageId(pageId)}
+					>
 						{page}
 					</button>
 				);
