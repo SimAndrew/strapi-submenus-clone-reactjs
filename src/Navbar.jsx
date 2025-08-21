bf71d48f76458eea3e2f0212d056f5b577af137c
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-08-21 20:43:47 +0300

    feat: hide Submenu when mouse cursor is outside of Submenu list

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,10 +3,15 @@ import { useGlobalContext } from './Context.jsx';
 import NavLinks from './NavLinks.jsx';
 
 function Navbar() {
-	const { openSidebar } = useGlobalContext();
+	const { openSidebar, setPageId } = useGlobalContext();
+	const handleSubmenu = (e) => {
+		if (!e.target.classList.contains('nav-link')) {
+			setPageId(null);
+		}
+	};
 
 	return (
-		<nav>
+		<nav onMouseOver={handleSubmenu}>
 			<div className="nav-center">
 				<h3 className="logo">strapi</h3>
 				<button className="toggle-btn" onClick={openSidebar}>
